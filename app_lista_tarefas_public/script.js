356daa409ed8ee76af74b069e01d89b4b7d46b3e
Yves Lima Silva <yveslimasilva@gmail.com>
2021-12-11 22:01:18 -0300

    adicionei os arquivos

---
+++ app_lista_tarefas_public/script.js
@@ -0,0 +1,45 @@
+function editar(id, txt_tarefa) {
+
+    let form = document.createElement('form');
+    form.action = "index.php?pag=index&acao=atualizar";
+    form.method = 'post';
+    form.className = 'row';
+
+    let inputTarefa = document.createElement('input');
+    inputTarefa.type = 'text';
+    inputTarefa.name = 'tarefa';
+    inputTarefa.className = 'col-9 form-control';
+    inputTarefa.value = txt_tarefa;
+
+    let inputId = document.createElement('input');
+    inputId.type = 'hidden';
+    inputId.name = 'id';
+    inputId.value = id;
+
+    let button = document.createElement('button');
+    button.type = 'submit';
+    button.className = 'btn col-3 btn-info';
+    button.innerHTML = 'atualizar';
+
+    form.appendChild(inputTarefa);
+    form.appendChild(inputId);
+    form.appendChild(button);
+    
+    let tarefa = document.getElementById('tarefa_'+id);
+
+    tarefa.innerHTML = '';
+
+    tarefa.insertBefore(form, tarefa[0]);
+
+}
+
+function remover(id) {
+    location.href = 'index.php?pag=index&acao=remover&id=' + id;
+
+}
+
+function marcarRealizada(id){
+    location.href = 'index.php?pag=index&acao=marcarRealizada&id=' + id;
+
+}
+
---
+++ app_lista_tarefas_public/tarefa_controller.php
@@ -0,0 +1,5 @@
+<?php
+
+ require_once '../app_lista_tarefas_privado/tarefa_controller.php';
+ 
+?>
